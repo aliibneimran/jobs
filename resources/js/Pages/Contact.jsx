1324372cb12a6c6dd51706fc10426314b192b6fc
unknown <aliibneimran@gmail.com>
2024-02-22 19:53:26 +0600

    add

--- resources/js/Pages/Contact.jsx
+++ resources/js/Pages/Contact.jsx
@@ -13,7 +13,7 @@ export default function Contact() {
               <div className="row">
                 <div className="col-lg-6">
                   <h2 className="mb-10">About Us</h2>
-                  <p className="font-lg color-text-paragraph-2">Get the latest news, updates and tips</p>
+                  <p className="font-lg color-text-paragraph-2">Get the latest news jjjbjdbj, updates and tips</p>
                 </div>
                 <div className="col-lg-6 text-lg-end">
                   <ul className="breadcrumbs mt-40">
